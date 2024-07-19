41e7ff4bce9bb6add52f8886cb99371cdc866807
fatima <fatimah20mohammed04@gmail.com>
2024-07-19 10:34:15 +0300

    done add 4 function

---
+++ workshop.py
@@ -0,0 +1,19 @@
+def say_hi():
+    print('hello ')
+say_hi()
+
+def ask_name():
+    o = input('enter your name ')
+    return o 
+
+ask_name()
+
+def sum_num():
+    print(1 + 99 )
+
+sum_num()
+
+def say_goodbuy():
+    print('buy buy')
+
+say_goodbuy()
